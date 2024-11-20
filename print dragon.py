31650c4cb58e5c296991f597a38986a247725e9b
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2024-11-20 21:30:40 +0530

    dragon

--- print dragon.py
+++ print dragon.py
@@ -1,16 +1,3 @@
-dragon_body = ['  @   ', ' / \  ', '(    )', '|""""|', '\(__)/']
-fire_parts = ['   *   ', '  ***  ', ' ***** ', '*******', ' ***** ', '  ***  ', '   *   ']
-
-for part in dragon_body:
-    print(part)
-
-print()  # Adding a blank line for separation
-
-for i in range(len(fire_parts)):
-    if i % 2 == 0:
-        print(' ' * (len(dragon_body[0]) // 2 - len(fire_parts[i]) // 2) + fire_parts[i])
-    else:
-        print(fire_parts[i])
 
 
 dragon = [
@@ -45,3 +32,4 @@ for line in dragon:
 # Print the fire
 for line in fire:
     print(line)
+
